560fd430fa04e248ad2b74b9196731645aab186f
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 12:20:15 +0100

    Create Exercise8.py

---
+++ Practical2nd/Exercise8.py
@@ -0,0 +1,7 @@
+# Converting between inches, and centimeters & feet
+
+heightinches = int(input("Please input your height in inches: "))
+print("Coverting into CM from inches: ")
+print(heightinches*2.54)
+print("So your height in feet is: ")
+print(heightinches/12)
