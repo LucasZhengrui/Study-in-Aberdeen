646d4f6e729864dc2218ce1da8d8f7b8e5b93e9d
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 09:57:41 +0100

    Create RIPQueen.py

---
+++ RIPQueen.py
@@ -0,0 +1,8 @@
+# Coding freestyle, but showing respect to the Queen Elizabeth II. Rest in peace.
+
+name = input("Please type your name here:")
+age = int(input("Please input your age here:"))
+year = 2022 - age - 1926
+msg = name + ", you are younger than Her Majesty about " + str(year)
+print(msg)
+print("God bless the Queen, Rest in peace. R.I.P")
