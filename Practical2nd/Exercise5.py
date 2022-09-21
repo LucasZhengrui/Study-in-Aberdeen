fa817443b028cbfacdc899da00a9db3efa2ce12b
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 11:53:07 +0100

    Create Exercise5.py

---
+++ Practical2nd/Exercise5.py
@@ -0,0 +1,6 @@
+# input the width and height and then show the area of this rectangle
+
+width = int(input("Please input the width of a rectangle: "))
+height = int(input("Please input the height of a rectangle: "))
+print("Here is the area of your rectangle: ")
+print(width*height)
