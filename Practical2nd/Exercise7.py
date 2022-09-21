10eb7ce80df01ad91a46f2008bbf1b93ebc47cfd
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 12:09:44 +0100

    Create Exercise7.py

---
+++ Practical2nd/Exercise7.py
@@ -0,0 +1,9 @@
+# Show distance of light C. in how many seconds you have input (Besides the speed of light C. is 3*(10^8) per seconds)
+
+seconds = int(input("Please input a time in seconds: "))
+print("Distance of light C. in meters")
+print(300000000*seconds)
+print("Distance of light C. in kilometers")
+print(300000*seconds)
+print("Distance of light C. in miles")
+print(300000000*seconds/1609)
