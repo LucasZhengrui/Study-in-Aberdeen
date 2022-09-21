f0617bc06155f1748999dd908393bed63520336b
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 09:46:16 +0100

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,6 @@
 # Main Exercise: 
 
-# Exercise 3: Create a program that asks the user to enter their name and their age. Print out a messageaddressed to them that tells them the year that they will turn 100 years old. Note: for this exercise,the expectation is that you explicitly write out the year (and therefore be out of date the next year).
+# Exercise 3: Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old. Note: for this exercise,the expectation is that you explicitly write out the year (and therefore be out of date the next year).
 # Exercise 4: Print out that 5 copies of the message of the program in Exercise 3 on separate lines. 
 # Exercise 5: Add on to the program in Exercise 3 by asking the user for another number and printing out thatmany copies of the previous message.
 
