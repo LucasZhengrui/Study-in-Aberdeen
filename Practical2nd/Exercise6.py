138d5ccda02a030dd543d642d54e926a5d32af4f
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 11:58:45 +0100

    Create Exercise6.py

---
+++ Practical2nd/Exercise6.py
@@ -0,0 +1,7 @@
+# input side of Square and then showing the area and perimeter
+
+side = int(input("Please input the side of your square: "))
+print("Perimeter of Square: ")
+print(side*4)
+print("Area of Square: ")
+print(side*side)
