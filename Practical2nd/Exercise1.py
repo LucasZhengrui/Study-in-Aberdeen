ba69c388038729ba32edc8420b195d5b4fcb451d
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-21 11:19:37 +0100

    Create Exercise1.py

---
+++ Practical2nd/Exercise1.py
@@ -0,0 +1,12 @@
+# input your first name, last name, age, salary, and postal code. finally display them in the result area.
+
+firstname = input("Please input your first name here, thanks: ")
+lastname = input("Please input your last name here thanks: ")
+age = input("And now, please input your age, appreciate: ")
+salary = input("Here is a private info - salary(Pounds), we will not share it with others, no worries: ")
+postalcode = input("Please input your postcode now, and please make sure that all of the characters are correct, cheers: ")
+print("Your first name is " + firstname)
+print("Your last name is " + lastname)
+print("Your age is " + age + " years old")
+print("Your salary(Private info warning) is " + salary + " Pounds")
+print("Lastly, your postalcode is " + postalcode)
