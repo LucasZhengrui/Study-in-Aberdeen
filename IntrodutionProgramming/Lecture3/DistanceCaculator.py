8c8fc092a4bca83b2ddc0de9a36225fa36c81279
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-22 09:28:31 +0100

    Update DistanceCaculator.py

--- IntrodutionProgramming/Lecture3/DistanceCaculator.py
+++ IntrodutionProgramming/Lecture3/DistanceCaculator.py
@@ -7,3 +7,7 @@ D = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 # D = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
 print("Distance between these two locations: ")
 print(D)
+
+
+# How to rename the library's name:
+# import math as (any names you want)
