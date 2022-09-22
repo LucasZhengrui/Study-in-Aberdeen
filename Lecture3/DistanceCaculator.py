5a2bb09515e89563755b4a859149a12177a6a272
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-22 09:21:15 +0100

    Create DistanceCaculator.py

---
+++ Lecture3/DistanceCaculator.py
@@ -0,0 +1,9 @@
+import math
+x1 = int(input("Please input the location 1's X"))
+y1 = int(input("Please input the location 1's Y"))
+x2 = int(input("Please input the location 2's X"))
+y2 = int(input("Please input the location 2's Y"))
+D = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+# D = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+print("Distance between these two locations: ")
+print(D)
