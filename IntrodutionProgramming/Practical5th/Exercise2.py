6d1586601691546df82a44c1669d7cf3cc38a64c
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-26 13:18:38 +0100

    Create Exercise2.py

---
+++ IntrodutionProgramming/Practical5th/Exercise2.py
@@ -0,0 +1,6 @@
+save_money = 0
+sum = 0
+for i in range(366): # 2020 have 366 days
+    save_money = save_money + 1
+    sum = sum + save_money
+print("The total money you have saved are", sum, "P, and the final day you should save", save_money, "P.")
