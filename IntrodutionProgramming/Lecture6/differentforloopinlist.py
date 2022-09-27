99c0acc72ee44bdf2c6cf63beddebcd1b95bebfb
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-27 16:41:42 +0100

    Create differentforloopinlist.py

---
+++ IntrodutionProgramming/Lecture6/differentforloopinlist.py
@@ -0,0 +1,8 @@
+# Check the difference between the first part and the second, pay attention to the different "for" statement
+
+# str = ["Hello", 1, "world"]
+# for i in range(len(str)):
+#     print(i, str[i])
+
+for i in str:
+    print(i)
