0384a84ba0f61af8afbf671dccd834bf305a3d62
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-27 16:23:59 +0100

    Update harderone.py

--- IntrodutionProgramming/Lecture6/changeeveryfirstchartodollar/harderone.py
+++ IntrodutionProgramming/Lecture6/changeeveryfirstchartodollar/harderone.py
@@ -1,9 +1,9 @@
-string_ = "Introduction of Python Programming"
-new_string = string_.split(" ")
+string_ = "Introduction of Python Programming" # string
+new_string = string_.split(" ") # change string to list
 new_list = []
 print(new_string)
 for i in new_string:
     new_list.append('$' + i[1:])
-x = ' '.join(new_list)
+x = ' '.join(new_list) # change list back to string
 
 print(x)
