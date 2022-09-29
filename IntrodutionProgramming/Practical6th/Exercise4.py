4cf769664b5b8a081481740ac53f62719e2bca57
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-29 21:06:52 +0100

    Update Exercise4.py

--- IntrodutionProgramming/Practical6th/Exercise4.py
+++ IntrodutionProgramming/Practical6th/Exercise4.py
@@ -1,20 +1,30 @@
-def security(mes):
-    if mes == "X":
-        mes = "A"
-        return mes
-    elif mes == "Y":
-        mes = "B"
-        return mes
-    elif mes == "Z":
-        mes = "C"
-        return mes
-    else:
-        mes = ord(mes) + 3
-    return chr(mes)
+temp = []
+number = 1
+while number == 1:
+    str = input("Please input your message, white space is for break: ")
+    for item in range(len(str)):
+        if str[item] == "X":
+            num = ord(str[item]) - 23
+            mess = chr(num)
+            temp.append(mess)
+            #message = ''.join(temp)
+        elif str[item] == "Y":
+            num = ord(str[item]) - 23
+            mess = chr(num)
+            temp.append(mess)
+            #message = ''.join(temp)
+        elif str[item] == "Z":
+            num = ord(str[item]) - 23
+            mess = chr(num)
+            temp.append(mess)
+            #message = ''.join(temp)
+        elif str[item] == " ":
+            number = 0
+            break
+        else:
+            num = ord(str[item]) + 3
+            mess = chr(num)
+            temp.append(mess)
+        message = ''.join(temp)
 
-while 1:
-    mes = input("Please inpiut your message: ")
-    if mes != " ":
-        print(security(mes))
-    else:
-        break
+print(message)
