3ec759bc2fc53e3b1bb2adb1bd59c74534579d5d
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-29 13:29:18 +0100

    Create Exercise2.py

---
+++ IntrodutionProgramming/Practical6th/Exercise2.py
@@ -0,0 +1,7 @@
+while 1:
+    values = int(input("Please input a number: "))
+    if values == 0:
+        print("Well, the negative number is ", values)
+        break
+    else:
+        print("All of postive values are ", values)
