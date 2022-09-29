803a21801b9af82c913e59c69054393f1ae5cfa5
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-29 21:17:11 +0100

    Update Exercise5(appendmodeforfile_additionaltopic).py

--- IntrodutionProgramming/Practical6th/Exercise5(appendmodeforfile_additionaltopic).py
+++ IntrodutionProgramming/Practical6th/Exercise5(appendmodeforfile_additionaltopic).py
@@ -1,4 +1,4 @@
-fh = open("example.txt", 'a') # Opening the file with append mode (Make sure you have an file in your database before run these codes already)
+fh = open("example.txt", 'a') # Opening the file with append mode (Make sure you have an 'example.txt' file in your database before run these codes already)
 
 fh.write("\nThis is line 3\nThis is line 4")
 
