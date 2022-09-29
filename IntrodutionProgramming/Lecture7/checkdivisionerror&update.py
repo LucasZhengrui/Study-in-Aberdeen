c95162c3ded45f048f18dab70d67295bb7b30c55
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-09-29 10:47:57 +0100

    Update checkdivisionerror&update.py

--- IntrodutionProgramming/Lecture7/checkdivisionerror&update.py
+++ IntrodutionProgramming/Lecture7/checkdivisionerror&update.py
@@ -15,3 +15,18 @@ result1 = div(3, 0)
 print(result1)
 result = div2(3, 0)
 print(result)
+
+# Best solution:
+# def func():
+#     while True:
+#         try:
+#             x = int(input("Please enter x: "))
+#             y = int(input("Please enter y: "))
+#             return x/y # If x and y are both correct, return can be ran, and break as well. Otherwise, it will skip to the except statement.
+#             break
+#         except:
+#             print("The last input is not a number or y is 0.")
+# 
+# func()
+#
+# # It's an example to show how to make sure the input values are correct(try and except)
