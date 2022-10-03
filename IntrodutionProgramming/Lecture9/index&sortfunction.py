330c067dd32c2077a219f94f241da132e9029b8c
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-10-03 10:34:01 +0100

    Create index&sortfunction.py

---
+++ IntrodutionProgramming/Lecture9/index&sortfunction.py
@@ -0,0 +1,15 @@
+fruits = ['cherry', 'banana', 'apple', 'banana', 'cherry', 'nub', 'jesus']
+
+number = [3, 4, 1, 2, 6, 0, 12]
+
+x = fruits.index("cherry") # Return No. x that "cherry" in where
+
+print(x)
+
+fruits.sort()
+
+number.sort()
+
+print(fruits)
+
+print(number)
