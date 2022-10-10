2ba851518819ab81752a343e410e1d33e10932c0
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-10-11 00:28:17 +0100

    Create Exercise1.sh

---
+++ InformationSecurity/Shell/Pratical1/Exercise1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Here is the bash shell script to show the current date, time, username and current directory.
+#
+clear
+echo "--------------------------------------------"
+echo "Now is \c" ; date
+echo "Well, your username is $USER."
+echo "Finally, the current directory is \c" ; pwd
+echo "Thank you so much!" # Just a test here
+# cal
