99944d9c36cae2a31f9411b5999e0e78ddd03287
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-10-19 11:57:00 +0100

    Create inserctdatatostafftable.sql

---
+++ DatabaseBigData/Pratical2/inserctdatatostafftable.sql
@@ -0,0 +1,66 @@
+INSERT INTO Staff
+VALUES(
+    '1',
+    'Noob',
+    'BigNub',
+    'Manager',
+    'Female',
+    '01-03-1995',
+    20000,
+    'BOO2'
+);
+INSERT INTO Staff
+VALUES(
+    '2',
+    'Boris',
+    'John',
+    'Director',
+    'Male',
+    '02-10-1980',
+    19000,
+    'BOO2'
+);
+INSERT INTO Staff
+VALUES(
+    '3',
+    'Donald',
+    'Trust',
+    'Senior Manager',
+    'Female',
+    '21-09-1985',
+    26000,
+    'BOO3'
+);
+INSERT INTO Staff
+VALUES(
+    '4',
+    'Ray',
+    'Lucas',
+    'CEO',
+    'Male',
+    '21-03-2000',
+    30000,
+    'BOO4'
+);
+INSERT INTO Staff
+VALUES(
+    '5',
+    'Eason',
+    'Lee',
+    'Manager',
+    'Male',
+    '23-04-1997',
+    27000,
+    'BOO3'
+);
+INSERT INTO Staff
+VALUES(
+    '6',
+    'Julia',
+    'Poly',
+    'Trainee',
+    'Female',
+    '01-03-2001',
+    15000,
+    'BOO2'
+);
