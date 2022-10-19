615a770b63cf2efa84c2dfcef7ead76ce0c1c56e
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-10-19 11:17:52 +0100

    Create inserctdatatobranchtable.sql

---
+++ DatabaseBigData/Pratical2/inserctdatatobranchtable.sql
@@ -0,0 +1,5 @@
+INSERT INTO Branch VALUES ('B002', '56 Clover Dr', 'London', 'NW10 6EU' );
+INSERT INTO Branch VALUES ('B003', '163 Main St', 'Glasgow', 'G11 9QX' );
+INSERT INTO Branch VALUES ('B004', '32 Manse Rd', 'Bristol', 'BS99 1NZ' );
+INSERT INTO Branch VALUES ('B005', '22 Deer Rd', 'London', 'SW1 4EH' );
+INSERT INTO Branch VALUES ('B007', '16 Argyll St', 'Aberdeen', 'AB2 3S' );
