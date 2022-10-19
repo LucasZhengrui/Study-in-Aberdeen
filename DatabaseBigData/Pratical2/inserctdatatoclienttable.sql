9c49ba0a3069891cc819c29277e3c95ec33414a2
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-10-19 11:24:47 +0100

    Create inserctdatatoclienttable.sql

---
+++ DatabaseBigData/Pratical2/inserctdatatoclienttable.sql
@@ -0,0 +1,4 @@
+INSERT INTO Client VALUES ('CR56', 'Aline', 'Stewart', '0141-848-1825', 'Flat', 350 );
+INSERT INTO Client VALUES ('CR62', 'Mary', 'Tregear', '01224-196720', 'Flat', 600 );
+INSERT INTO Client VALUES ('CR74', 'Mike', 'Ritchie', '01475-392178', 'House', 750 );
+INSERT INTO Client VALUES ('CR76', 'John', 'Kay', '0207-774-5632', 'Flat', 425 );
