15bda324e40ed968dd8e615fb8b3eac3a1470655
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-01 12:14:40 +0000

    Create decrypt.py

---
+++ InformationSecurity/Practical4/decrypt.py
@@ -0,0 +1,22 @@
+def dencrypt(text, s):
+    # result = []
+    result_str=''
+    # transverse the plaintext
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plaintext
+      
+        if char.upper: # isupper() is Verifying whether the characters are all of upper letters
+            result_str += chr((ord(char)- 97 - s) % 26 + 97) # ord() is transfer characters to their ASCII code number, the reverse is chr()
+        # Encrypt lowercase characters in plaintext
+        else:
+            result_str +=  chr((ord(char) - 65 - s) % 26 + 65)
+    return result_str
+
+#check the above function
+text = input("Please input your Cipher: ")
+s = int(input("Please input the shift pattern number: "))
+
+print("Cipher: ", text)
+print("PlainText", dencrypt(text, s))
+print("Shift pattern: ", s)
