dbec89b049cb07778daecf32a1f251725bcc1bb5
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 15:52:53 +0000

    Create PaintingTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/DataInsert/PaintingTableDataInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO painting VALUES (1, 'watercolor', 'paper', 'abstract');
+INSERT INTO painting VALUES (2, 'oil', 'canvas', 'modern');
+INSERT INTO painting VALUES (5, 'watercolor', 'paper', 'classic');
