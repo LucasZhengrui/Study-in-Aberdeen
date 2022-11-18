ced7c661b7f36dd832bfe4b938c1d82e043181f2
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 15:11:26 +0000

    Create ArtCuratorTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/ArtCuratorTableDataInsert.sql
@@ -0,0 +1,6 @@
+INSERT INTO artcurator VALUES (1, 1);
+INSERT INTO artcurator VALUES (2, 2);
+INSERT INTO artcurator VALUES (3, 1);
+INSERT INTO artcurator VALUES (4, 3);
+INSERT INTO artcurator VALUES (5, 4);
+INSERT INTO artcurator VALUES (6, 5);
