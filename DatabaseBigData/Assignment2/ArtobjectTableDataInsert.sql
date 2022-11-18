29e2ac2d339a1e2687785e84dd45f50f27ad9580
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 14:53:58 +0000

    Create ArtobjectTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/ArtobjectTableDataInsert.sql
@@ -0,0 +1,6 @@
+INSERT INTO artobject VALUES (1, 2, '1950-05-01', 'Smile', 'Just a smile from a man', 'United Kindom', 'Victoria', '2000-03-23', 10000.2, 'On hold', 'Painting');
+INSERT INTO artobject VALUES (2, 1, '1940-03-23', 'Cry', 'Just cry from a woman', 'United States', 'Medium Century', '2001-02-22', 15000.2, 'Showing', 'Painting');
+INSERT INTO artobject VALUES (3, 4, '1800-06-04', 'Freedom', 'Just a statue of freedom', 'France', 'Medium Century', '1950-03-03', 20000.2, 'Showing', 'Statue');
+INSERT INTO artobject VALUES (4, 4, '1810-09-01', 'Winner', 'Just a statue of Winner', 'Russia', 'Tsar Epoch', '1980-03-03', 25000.2, 'In progress', 'Statue');
+INSERT INTO artobject VALUES (5, 3, '1920-10-01', 'Horse', 'Running Horses', 'German', 'Medium Century', '1999-03-13', 20000.2, 'Showing', 'Painting');
+INSERT INTO artobject VALUES (6, 5, '2000-10-01', 'Countryside', 'Farming', 'Italia', 'Medium Century', '2000-12-31', 30000.2, 'In progress', 'Other');
