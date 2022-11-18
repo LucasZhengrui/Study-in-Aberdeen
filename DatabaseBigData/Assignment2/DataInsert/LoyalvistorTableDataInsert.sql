dc3a702586933ff1246e6983e146df3ab2278caf
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 18:07:22 +0000

    Create LoyalvistorTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/DataInsert/LoyalvistorTableDataInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO loyalvistor VALUES ('Lucas', 'Jay', 1, '123456', 'King Street', 'Aberdeen', 'AB33 555', 'Oil Painting');
+INSERT INTO loyalvistor VALUES ('Sue', 'Hubs', 2, '654321', 'High Street', 'Glasgow', 'GS25 444', 'Stone Statue');
+INSERT INTO loyalvistor VALUES ('Bobi', 'Hanker', 3, '198964', 'George Street', 'London', 'LD44 888', 'Every Type');
