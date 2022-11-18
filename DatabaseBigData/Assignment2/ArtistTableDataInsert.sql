c8367fe8038af1fc14c6d84c8afb29cb66dae3a4
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 14:16:53 +0000

    Create ArtistTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/ArtistTableDataInsert.sql
@@ -0,0 +1,5 @@
+INSERT INTO artist VALUES (1, 'Joe', 'John', '1900-10-01', '1990-01-10');
+INSERT INTO artist VALUES (2, 'Tony', 'Wang', '1910-11-11', '1999-09-10');
+INSERT INTO artist VALUES (3, 'Johnson', 'Poly', '1890-10-10', '1950-06-04');
+INSERT INTO artist VALUES (4, 'Willam', 'Park', '1790-10-10', '1840-06-04');
+INSERT INTO artist VALUES (5, 'Bob', 'Owen', '1990-10-10', null);
