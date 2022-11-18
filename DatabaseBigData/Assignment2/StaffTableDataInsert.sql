3cbf4ddaab83f0f4ce9527b19350523b224f93fa
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 15:07:25 +0000

    Create StaffTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/StaffTableDataInsert.sql
@@ -0,0 +1,5 @@
+INSERT INTO staff VALUES (1, 'Isham', 'Park', 'Senior Manager');
+INSERT INTO staff VALUES (2, 'Lucas', 'Raymond', 'Manager');
+INSERT INTO staff VALUES (3, 'Jason', 'Ham', 'Supervisor');
+INSERT INTO staff VALUES (4, 'Donald', 'Boris', 'Supervisor');
+INSERT INTO staff VALUES (5, 'Jay', 'Jacky', 'Manager');
