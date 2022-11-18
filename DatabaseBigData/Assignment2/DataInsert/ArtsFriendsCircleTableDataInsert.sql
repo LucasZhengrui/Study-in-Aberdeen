c3ac6c05520afa02797713e3a2a46d75563b0ffb
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-18 22:13:33 +0000

    Create ArtsFriendsCircleTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/DataInsert/ArtsFriendsCircleTableDataInsert.sql
@@ -0,0 +1,4 @@
+INSERT INTO artsfriendscircle VALUES ('His Majesty M.', Null , 'King Street', 'Cardiff', 'CD25 3DS', '07426262', 'John Stephen');
+INSERT INTO artsfriendscircle VALUES ('St. George M.', 'Charity M.' , 'George Street', 'Glasgow', 'GS25 3TS', '07436464', 'Kimi Fox');
+INSERT INTO artsfriendscircle VALUES ('Queen M.', 'EQ Memorial' , 'Queen Street', 'London', 'LD35 3TX', '00014646', 'Jack Johnson');
+INSERT INTO artsfriendscircle VALUES ('Aberdeen M.', 'City Coucil_s' , 'High Street', 'Aberdeen', 'AB55 2YX', '07418964', 'Stark Joe');
