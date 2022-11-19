84cbff6a5eb1be52ecb68ec6f9a6c79316a7d235
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-19 01:12:51 +0000

    Update ExhibitionTableDataInsert.sql

--- DatabaseBigData/Assignment2/DataInsert/ExhibitionTableDataInsert.sql
+++ DatabaseBigData/Assignment2/DataInsert/ExhibitionTableDataInsert.sql
@@ -1,3 +1,3 @@
-INSERT INTO exhibition VALUES (1, 'Kings Exhibition', 'modern', 'Celebration', '2022-05-04', '2022-06-04');
-INSERT INTO exhibition VALUES (2, 'Painting show', 'Mixed', 'painting chance', '2022-12-25', '2023-03-25');
-INSERT INTO exhibition VALUES (3, 'Special SS', 'Stone, wood', 'Reunion', '2022-01-01', '2022-12-31');
+INSERT INTO exhibition VALUES (1, 'Kings Exhibition', 'featured', 'Celebration', '2022-05-04', '2022-06-04');
+INSERT INTO exhibition VALUES (2, 'Painting show', 'regular', 'painting chance', '2022-12-25', '2023-03-25');
+INSERT INTO exhibition VALUES (3, 'Special SS', 'featured', 'Reunion', '2022-01-01', '2022-12-31');
