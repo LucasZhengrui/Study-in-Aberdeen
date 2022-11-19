c02e599850fe398aef91114afa0534589e1d525b
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-19 01:25:00 +0000

    Update 1.sql

--- DatabaseBigData/Assignment2/Transaction_Requirements/1.sql
+++ DatabaseBigData/Assignment2/Transaction_Requirements/1.sql
@@ -1,3 +1,5 @@
+# 1. Add a new art object to the museum's permanent collection
+
 insert into artobject values (
 7, 1, '1955-07-01', 'Tower Bridge', 'Bridge in London', 'United Kindom', 'Modern', '2022-11-18', 20000.0, 'in store', 'Queen M.', 'Other'
 );
