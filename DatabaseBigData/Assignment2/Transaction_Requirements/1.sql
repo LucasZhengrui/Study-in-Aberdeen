3a77e2eed0ed1b6bcce2d7ea3fb29463ee03df13
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-19 01:24:27 +0000

    Create 1.sql

---
+++ DatabaseBigData/Assignment2/Transaction_Requirements/1.sql
@@ -0,0 +1,8 @@
+insert into artobject values (
+7, 1, '1955-07-01', 'Tower Bridge', 'Bridge in London', 'United Kindom', 'Modern', '2022-11-18', 20000.0, 'in store', 'Queen M.', 'Other'
+);
+insert into other values (
+7, 'photo', 'modern'
+);
+
+select * from artobject;
