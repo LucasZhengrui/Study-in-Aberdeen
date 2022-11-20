6f65de62378778340274c75cbf3264624e3aa0f2
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2022-11-20 01:26:42 +0000

    Create ArtobjectborrowedTableDataInsert.sql

---
+++ DatabaseBigData/Assignment2/DataInsert/ArtobjectborrowedTableDataInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO artobjectborrowed VALUES (1, 1, '2022-11-01', '2023-10-31');
+INSERT INTO artobjectborrowed VALUES (2, 4, '2022-10-01', '2023-9-30');
+INSERT INTO artobjectborrowed VALUES (3, 6, '2022-11-19', '2023-11-18');
