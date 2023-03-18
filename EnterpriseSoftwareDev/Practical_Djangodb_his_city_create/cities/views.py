76bcf641e906534edb6a1382653ddc9a11ff1411
Lucas <lucaszhengrui@gmail.com>
2023-03-18 18:13:11 +0000

    install pymysql and start to write insert def

--- EnterpriseSoftwareDev/Practical_Djangodb_his_city_create/cities/views.py
+++ EnterpriseSoftwareDev/Practical_Djangodb_his_city_create/cities/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import City
-from .forms import City_form
+import pymysql
+# from .forms import City_form
 # Create your views here.
+
 def city_list(request):
         cities = City.cities()
         paginator = Paginator(cities, 50) #show 50 cities per page
@@ -17,7 +19,17 @@ def city__by_name(request, city):
         page_obj = paginator.get_page(page_number)
         return render(request, 'cities/city_list.html', {'page_obj': page_obj})
 
-def city_edit(request, city):
-        update_stuff = City.objects.get(pk = city)
-        form = City_form
-        return render(request, 'cities/edit.html', {'form': form})
+# def city_edit(request, city):
+#         update_stuff = City.objects.get(pk = city)
+#         form = City_form
+#         return render(request, 'cities/edit.html', {'form': form})
+
+def data_insert(request):
+        if request.method == 'GET':
+                return render(request, 'cities/edit.html')
+        else:
+                input_value = request.POST.get('title')
+                connection = pymysql.connect(host = '127.0.0.1', port = 8000, db = 'db.sqlite3', charset = 'utf8')
+                cursor = connection.cursor(cursor = pymysql.cursors.DictCursor)
+
+                # cursor.execute('insert')
