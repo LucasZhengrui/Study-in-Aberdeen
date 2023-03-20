1c19f04c05516778703ed4c5d073906dd3dda249
Lucas <lucaszhengrui@gmail.com>
2023-03-20 10:20:40 +0000

    Second commit

--- EnterpriseSoftwareDev/Django_cruise_agent/cruises/views.py
+++ EnterpriseSoftwareDev/Django_cruise_agent/cruises/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Cruise, Ship, Cabin
+
+# Create your views here.
+def index(request):
+    cruises = Cruise.objects.all()
+    return render(request, 'cruises/index.html', {'cruises': cruises})
