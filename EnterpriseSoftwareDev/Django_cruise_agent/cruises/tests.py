7a85a838f1f0fe3eb7a59425497d7b1d9dc38420
Lucas <lucaszhengrui@gmail.com>
2023-03-20 14:05:32 +0000

    fix fixtures file but could not run the test in the Windows OS

--- EnterpriseSoftwareDev/Django_cruise_agent/cruises/tests.py
+++ EnterpriseSoftwareDev/Django_cruise_agent/cruises/tests.py
@@ -12,4 +12,4 @@ class CruiseTests(TestCase):
         #print(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Cruise")
-        self.assertContains(response,"MorrisWolfe")
+        self.assertContains(response,"Webster PLC")
