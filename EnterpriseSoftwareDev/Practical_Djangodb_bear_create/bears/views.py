673dc6091dca0c6ae40ae28b9bb6506d4bbc55e7
Lucas <lucaszhengrui@gmail.com>
2023-03-27 00:05:39 +0100

    Update view file

--- EnterpriseSoftwareDev/Practical_Djangodb_bear_create/bears/views.py
+++ EnterpriseSoftwareDev/Practical_Djangodb_bear_create/bears/views.py
@@ -6,7 +6,21 @@ from django.urls import reverse_lazy
 # Create your views here.
 def bear_list(request):
     bears = Bear.objects.all()
-    return render(request, 'bears/bear_list.html', {'bears' : bears})
+    left_ear = 0
+    right_ear = 0
+    male = 0
+    female = 0
+
+    for bear in bears:
+        if bear.sex == 'M':
+            male += 1
+        else:
+            female += 1
+        if bear.ear_applied == 'Left':
+            left_ear += 1
+        else:
+            right_ear += 1
+    return render(request, 'bears/bear_list.html', {'bears' : bears, 'left_ear': left_ear, 'right_ear': right_ear, 'male': male, 'female': female})
 
 def bear_detail(request, id):
     # bears = get_object_or_404(Bear, id=id)
