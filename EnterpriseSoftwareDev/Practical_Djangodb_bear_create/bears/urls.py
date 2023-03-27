b7186eaafabbe8d12c8412a5924c8d0cdfab0cb9
Lucas <lucaszhengrui@gmail.com>
2023-03-27 13:54:40 +0100

    Added edit and create features sucessfully

--- EnterpriseSoftwareDev/Practical_Djangodb_bear_create/bears/urls.py
+++ EnterpriseSoftwareDev/Practical_Djangodb_bear_create/bears/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.bear_list, name='bears_list'),
-    path('bear/<int:id>', views.bear_detail, name= 'bear_detail'),
-    # path('edit/<int:id>', views.BearUpdate.as_view(), name='bear_edit'),
+    path('bear/<int:id>/', views.bear_detail, name= 'bear_detail'),
+    path('bear_new/', views.bear_new, name = 'bear_new'),
+    path('bear/<int:id>/edit/', views.bear_edit, name='bear_edit'),
+    path('bear/<int:id>/delete/', views.bear_delete, name='bear_delete'),
     ]
